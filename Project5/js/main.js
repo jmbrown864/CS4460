bc741199f38fc72243677e6f38a6c1d00de40ded
Groover, Ginna <ggroover3@gatech.edu>
2017-11-28 21:57:47 -0500

    Moved map into separate file, added header, added svg for timeline

--- Project5/js/main.js
+++ Project5/js/main.js
@@ -1,43 +1,27 @@
-svg = d3.select("svg");
-width = svg.attr("width");
-height = svg.attr("height");
-path = d3.geoPath()
-        .projection(
-            d3.geoMercator()
-            .scale(160)
-            .translate( [(width / 2), (height/2)] ));
+var padding = {t: 40, r: 40, b: 40, l: 40};
+mapSVG = d3.select(".map");
+mapWidth = mapSVG.attr("width");
+mapHeight = mapSVG.attr("height");
+mapSVG.attr("transform", "translate(" + [((window.innerWidth - mapWidth)/2), ((window.innerHeight - mapHeight)/2)] + ")");
+
+timelineSVG = d3.select(".timeline");
+timelineWidth = timelineSVG.attr("width");
+timelineHeight = timelineSVG.attr("height");
+timelineSVG.attr("transform", "translate(" + [((window.innerWidth - timelineWidth)/2), ((window.innerHeight - timelineHeight)/2)] + ")");
 
 var tip = d3.tip()
-            .attr('class', 'd3-tip')
-            .offset([0, 0])
-            .html(function(d) {
-              return "<span class='details'>" + d.properties.name +"</span>";
-            });
+    .attr('class', 'd3-tip')
+    .offset([0, 0])
+    .html(function(d) {
+      return "<span class='details'>" + d.properties.name +"</span>";
+    });
 
-svg.call(tip);
+mapSVG.call(tip);
 
 d3.json("./data/world-countries.json", function(mapJsonError, world) {
     if (mapJsonError) throw mapJsonError;
-    var countries = topojson.feature(world, world.objects.countries1).features,
-      neighbors = topojson.neighbors(world.objects.countries1.geometries);
-
-  svg.selectAll(".country")
-    .data(countries)
-    .enter().insert("path", ".graticule")
-    .attr("class", "country")
-    .attr("d", path)
-    .on('mouseover',function(d){
-        tip.show(d);
-    })
-    .on('mouseout', function(d){
-        tip.hide(d);
-    });
-
-  svg.insert("path", ".graticule")
-      .datum(topojson.mesh(world, world.objects.countries1, function(a, b) { return a !== b; }))
-      .attr("class", "boundary")
-      .attr("d", path)
-      .style("stroke", "#fff");
+    var map = new Map(102, [(mapWidth / 2 - 12), (mapHeight/2)]);
+    map.init(mapSVG, world, tip);
 
     d3.csv('./data/nobel_laureates.csv', function(error, data) {
         if (error) throw error;
--- Project5/js/map.js
+++ Project5/js/map.js
@@ -1,9 +1,39 @@
-function Map(width, height) {
-    this.width = width;
-    this.height = height;
+function Map(scale, translation) {
+    this.scale = scale;
+    this.translation = translation;
 }
 
-Map.prototype.init = function(world, svg) {
+Map.prototype.init = function(g, world, tip) {
+    var countries = topojson.feature(world, world.objects.countries1).features,
+      neighbors = topojson.neighbors(world.objects.countries1.geometries);
+
+    path = d3.geoPath()
+        .projection(
+            d3.geoMercator()
+            .scale(this.scale)
+            .translate(this.translation));
+
+    g.selectAll(".country")
+    .data(countries)
+    .enter().insert("path", ".graticule")
+    .attr("class", "country")
+    .attr("d", path)
+    .style('fill', '#7d899b')
+    .on('mouseover',function(d){
+        tip.show(d);
+    })
+    .on('mouseout', function(d){
+        tip.hide(d);
+    });
+
+    g.insert("path", ".graticule")
+        .datum(topojson.mesh(world, world.objects.countries1, function(a, b) { return a !== b; }))
+        .attr("class", "boundary")
+        .attr("d", path)
+        .style('stroke', 'white')
+        .style('fill', '#7d899b')
+        .style('stroke-width', 1.3);
+
 }
 
 Map.prototype.update = function(g, dataset) {
