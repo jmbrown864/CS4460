1341f030b39f940362bcfcaae21e623333c5e4b8
Groover, Ginna <ggroover3@gatech.edu>
2017-12-01 00:03:52 -0500

    Added treemap
    Basic treemap, need to add auto font sizing, less children displayed
    (maybe zoomable)

--- Project5/js/main.js
+++ Project5/js/main.js
@@ -1,103 +1,128 @@
 var padding = {t: 40, r: 40, b: 40, l: 40};
-var map = d3.select('#map');
-var mapWidth = +map.attr('width');
-var mapHeight = +map.attr('height');
+var treemapSVG = d3.select('#treemap');
+var treemapWidth = +treemapSVG.attr('width');
+var treemapHeight = +treemapSVG.attr('height');
+treemapSVG.attr("transform", "translate(" + [((window.innerWidth - treemapWidth)/2), ((window.innerHeight - treemapHeight)/2)] + ")");
 
-var atlLatLng = new L.LatLng(27.809928, -39.902344);
-var myMap = L.map('map').setView(atlLatLng, 2);
-
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 10,
-    minZoom: 1,
-    id: 'mapbox.light',
-    accessToken: 'pk.eyJ1IjoiamFnb2R3aW4iLCJhIjoiY2lnOGQxaDhiMDZzMXZkbHYzZmN4ZzdsYiJ9.Uwh_L37P-qUoeC-MBSDteA'
-}).addTo(myMap);
-
-console.log(myMap);
-
-var mapSVGLayer = L.svg();
-mapSVGLayer.addTo(myMap);
-var mapSVG = d3.select('#map').select('svg');
-var personaLinkG = mapSVG.select('g')
-    .attr('class', 'leaflet-zoom-hide');
+var treemap = d3.treemap()
+    .tile(d3.treemapResquarify)
+    .size([treemapWidth, treemapHeight])
+    .round(true)
+    .paddingInner(1);
 
 timelineSVG = d3.select(".timeline");
 timelineWidth = timelineSVG.attr("width");
 timelineHeight = timelineSVG.attr("height");
 timelineSVG.attr("transform", "translate(" + [((window.innerWidth - timelineWidth)/2), ((window.innerHeight - timelineHeight)/2)] + ")");
 
-globalCountryCenters = null;
+d3.csv('./data/nobel_laureates.csv', function(error, data) {
+    if (error) throw error; 
 
-d3.json('./data/ne_10m_admin_0_label_points-country.json', function(error, data) {
-
-    var countryCenters = d3.nest().key(function(d) {
-        return compress(d.properties.sr_subunit);
+    var nlByAffiliation = d3.nest().key(function(d) {
+        return d.category;
+    }).key(function(e) {
+        return e.affiliation;
     }).rollup(function(v) {
-        return v[0].geometry.coordinates;
-    })
-    .object(data.features);
+        return v.length;
+    }).entries(data)
+    .sort(function(a, b) {
+        return d3.sum(b.values, function(c) { return c.value; }) - d3.sum(a.values, function(c) { return c.value; });
+    });
+    nlByAffiliation.forEach((category) => {
+        category.values.sort(function(a, b) {
+            return b.value - a.value;
+        });
+    });
 
-    console.log(countryCenters);
-    console.log(countryCenters['usa']);
-    globalCountryCenters = countryCenters;
-    drawPersonas(countryCenters);
+    nlByAffiliation = {"key": "Nobel Laureates", "values": nlByAffiliation}; 
+    var nlByAffiliation = { "name": "Nobel Laureates", "children":
+        nlByAffiliation.values.map( function(category) {
+            return { "name": category.key, "children": 
+              category.values.map( function(aff) {
+                 return { "name": aff.key, "size": aff.value };
+              }) 
+            }; 
+        })
+    }; 
+
+    var fader = function(color) { return d3.interpolateRgb(color, "#fff")(0.2); },
+    color = d3.scaleOrdinal(d3.schemeCategory20.map(fader)),
+    format = d3.format(",d");
+
+    var root = d3.hierarchy(nlByAffiliation)
+        .eachBefore(function(d) { d.data.id = (d.parent ? d.parent.data.id + "." : "") + d.data.name; })
+        .sum(sumBySize)
+        .sort(function(a, b) { return b.height - a.height || b.value - a.value; });
+
+    treemap(root);
+
+    var cell = treemapSVG.selectAll("g")
+    .data(root.leaves())
+    .enter().append("g")
+      .attr("transform", function(d) { return "translate(" + d.x0 + "," + d.y0 + ")"; });
+
+    cell.append("rect")
+      .attr("id", function(d) { return d.data.id; })
+      .attr("width", function(d) { return d.x1 - d.x0; })
+      .attr("height", function(d) { return d.y1 - d.y0; })
+      .attr("fill", function(d) { return color(d.parent.data.id); });
+
+  cell.append("clipPath")
+      .attr("id", function(d) { return "clip-" + d.data.id; })
+    .append("use")
+      .attr("xlink:href", function(d) { return "#" + d.data.id; });
+
+  cell.append("text")
+      .attr("clip-path", function(d) { return "url(#clip-" + d.data.id + ")"; })
+    .selectAll("tspan")
+      .data(function(d) { return d.data.name.split(/(?=[A-Z][^A-Z])/g); })
+    .enter().append("tspan")
+      .attr("x", 4)
+      .attr("y", function(d, i) { return 13 + i * 10; })
+      .text(function(d) { return d; });
+
+    cell.append("title")
+        .text(function(d) { return d.data.id + "\n" + format(d.value); });
+
+  d3.selectAll("input")
+      .data([sumBySize, sumByCount], function(d) { return d ? d.name : this.value; })
+      .on("change", changed);
+
+var timeout = d3.timeout(function() {
+    d3.select("input[value=\"sumByCount\"]")
+        .property("checked", true)
+        .dispatch("change");
+  }, 2000);
+
+
+function changed(sum) {
+    timeout.stop();
+
+    treemap(root.sum(sum));
+
+    cell.transition()
+        .duration(750)
+        .attr("transform", function(d) { return "translate(" + d.x0 + "," + d.y0 + ")"; })
+      .select("rect")
+        .attr("width", function(d) { return d.x1 - d.x0; })
+        .attr("height", function(d) { return d.y1 - d.y0; });
+
+    //cell.selectAll("text").each(getSize).style("font-size", function(d) { return d.scale + 4 + "px"; });
+  }
+    
 });
 
-function drawPersonas(countryCenters) {
-    console.log("in drawPersonas");
-    d3.csv('./data/nobel_laureates.csv', function(error, data) {
-        if (error) throw error;      
-
-        personaLinkG.selectAll('.grid-node')
-            .data(data)
-            .enter().append('circle')
-            .attr('class', 'grid-node')
-            .style('fill', 'blue')
-            .style('fill-opacity', 0.6)
-            .attr('r', 2);
-
-        myMap.on('zoomend', updateLayers);
-        updateLayers();
-    });
+function sumBySize(d) {
+  return d.size;
 }
 
-function updateLayers() {
-    personaLinkG.selectAll('.grid-node')
-        .attr('cx', function(d){
-            return myMap.latLngToLayerPoint([+d['born_country_lat'], +d['born_country_lng']]).x;
-        })
-        .attr('cy', function(d){
-            return myMap.latLngToLayerPoint([+d['born_country_lat'], +d['born_country_lng']]).y;
-        });
+function sumByCount(d) {
+  return d.children ? 0 : 1;
 }
 
-function compress(countryName) {
-    return countryName.match(/\w/g).join("").toLowerCase();
+function getSize(d) {
+  var bbox = this.getBBox(),
+      cbbox = this.parentNode.getBBox(),
+      scale = Math.min(cbbox.width/bbox.width, cbbox.height/bbox.height);
+  d.scale = scale;
 }
-
-
-
-/*d3.json("./data/world-countries.json", function(mapJsonError, world) {
-    if (mapJsonError) throw mapJsonError;
-    var countries = topojson.feature(world, world.objects.countries1).features,
-      neighbors = topojson.neighbors(world.objects.countries1.geometries);
-    var map = new Map(133, [(mapWidth / 2 - 11), (mapHeight/2 - 12)]);
-    map.init(mapSVG, world, countries, neighbors);
-
-    d3.csv('./data/nobel_laureates.csv', function(error, data) {
-        if (error) throw error;
-
-        var laureatesByBornCountry = d3.nest()
-            .key(function(d) { return d.born_country; })
-            .object(data);
-
-        map.update();
-
-        var timeslide = new Timeslide();
-        timeslide.init(new Date("2010-06-26"), new Date("2017-06-26"), 600);
-
-
-    });
-});*/
-
